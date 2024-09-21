ce04f794afc3a0050465333e1317d84ceeb060af
richardktran.dev <richardktran.dev@gmail.com>
2024-09-21 14:04:43 +0700

    setup project skeleton

---
+++ cmd/web/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		infoLog:  infoLog,
+		errorLog: errorLog,
+	}
+
+	srv := &http.Server{
+		Addr:     *addr,
+		Handler:  app.routes(),
+		ErrorLog: errorLog,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/richardktran/snippetbox
+
+go 1.22.4
